111e46236a54e16d651a8108349f5082275c7584
Deore <=>
2019-01-04 09:46:56 +0530

    updates Date Function and Properties File

--- src/main/java/com/capgemini/app/App.java
+++ src/main/java/com/capgemini/app/App.java
@@ -12,7 +12,15 @@ public class App
     {
     	ApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
     	Organization orgnization = (Organization) context.getBean("organization");
-    	System.out.println(orgnization.toString());
-    	System.out.println("IPAdresses : " +orgnization.toString());
+		/* System.out.println(orgnization.toString()); */
+    	System.out.println("Name : "+orgnization.getName());
+    	System.out.println("OrgId : "+orgnization.getOrgId());
+    	System.out.println("Share Value : "+orgnization.getShareValue());
+    	System.out.println("DateOfEstablishment : "+orgnization.getDateOfEstablishment());
+    	System.out.println("IPAdresses : " +orgnization.getIpAddresses());
+    	System.out.println("cities : "+orgnization.getBoardMembers());
+    	System.out.println("Board Members : "+orgnization.getBoardMembers());
+    	System.out.println("Branch Managers : "+orgnization.getBranchManagers());
+    	
     }
 }
