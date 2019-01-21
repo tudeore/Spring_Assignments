e941ba278aa1d019832f96c8a346d9bc103fe838
Deore <=>
2019-01-21 20:05:46 +0530

    updates transaction assignment

--- bank-transection/src/main/java/com/cg/banktransection/transaction/TransactionType.java
+++
@@ -1,8 +0,0 @@
-package com.cg.banktransection.transaction;
-
-public interface TransactionType {
-
-	public static final String WITHDRAW = "Debit";
-	public static final String DEPOSIT = "Credit";
-
-}
